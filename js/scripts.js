86c9c899b57886de801920c462977548d62b5af2
Allison Sadin <aesadin@gmail.com>
2020-06-05 13:35:07 -0700

    create function that returns result of programming match after user completes  form

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,21 @@ $(document).ready(function() {
     const appeal = $("input:radio[name=appeal]:checked").val();
     const time = $("input:radio[name=time]:checked").val();
 
-
-
+    if (company === service && service === newTo) {
+      $(".match").text(company);
+      $("#result").show();
+    } else if (service === newTo && newTo === appeal) {
+      $(".match").text(service);
+      $("#result").show();
+    } else if (newTo === appeal && newTo === time) {
+      $(".match").text(newTo);
+      $("#result").show();
+    } else {
+      $(".match").text("Javascript");
+      $("#result").show();
+    }
+  });
+});
 
 
     $("#story").show();
