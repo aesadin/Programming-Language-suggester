0550ef5c9c80052c9c1fb27dbfc714a26c660a02
Allison Sadin <aesadin@gmail.com>
2020-06-05 12:32:18 -0700

    define variables used for name of eache radio button section

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    const company = $("input:radio[name=company]:checked").val();
+    const service = $("input:radio[name=service]:checked").val();
+    const newTo = $("input:radio[name=newTo]:checked").val();
+    const appeal = $("input:radio[name=appeal]:checked").val();
+    const time = $("input:radio[name=time]:checked").val();
+
+
 
 
 
